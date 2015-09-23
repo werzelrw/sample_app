634e1e1bdabae2ecdb88a82d1c7e3ec385c9a5cc
werzelrw <werzelrw@gmail.com>
2015-09-23 16:37:31 -0300

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,6 +32,20 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)
