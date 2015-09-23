76d03e703b6322a5c58cea3a04415791f14a0690
werzelrw <werzelrw@gmail.com>
2015-09-23 11:51:19 -0300

    Make a basic User model (including secure passwords)

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+	FILL_IN="Ruby on Rails Tutorial Sample App"
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+  end
+end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,5 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
