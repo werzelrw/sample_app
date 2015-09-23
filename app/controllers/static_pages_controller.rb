9d371141fa89b26d2e7c649124c2b805789d06da
werzelrw <werzelrw@gmail.com>
2015-09-23 10:29:08 -0300

    Eliminate repetition (solves exercise 3.1)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,19 @@
 class StaticPagesController < ApplicationController
+
   def home
+  	@base_title="Ruby on Rails Tutorial Sample App"
   end
 
   def help
+  	@base_title="Ruby on Rails Tutorial Sample App"
   end
 
   def about
+  	@base_title="Ruby on Rails Tutorial Sample App"
+  end
+
+  def contact
+  	@base_title="Ruby on Rails Tutorial Sample App"
   end
 
 end
